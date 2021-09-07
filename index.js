f20293b5a017c10c9bbd93df52c5076d37ea3fff
Jonathan Valencia <jonathan.valencia@prestti.com>
2021-09-06 22:29:54 -0500

    upgrade generater signal

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ global.mongodb = require("./helpers/mongodb.js");
 const _Binance = require('node-binance-api');
 const binance = new _Binance();
 const markettableList = require('./dao/marketable_list');
-let second_initial = 0,
-    second_counter = 0,
-    inside = false,
-    date_created;
+let inside = false;
 
 const sleep = async (time) => {
     return new Promise((resolve) => {
@@ -20,29 +17,33 @@ const timeToCallMarket = async () => {
 
     date_created = new Date();
     let initialSecond = date_created.getSeconds();
-    if (initialSecond <= 3)
-        binanceSaveMarketWithSocket();
+
+    if (initialSecond <= 5 || (initialSecond >= 30 && initialSecond <= 35)) {
+        inside = false;
+
+        const marketables = await markettableList.getAllActualMarketable();
+        binanceSaveMarketWithSocket(marketables);
+        stopSocket();
+    }
     else {
         await sleep(1000);
         timeToCallMarket();
     }
 };
 
-async function binanceSaveMarketWithSocket() {
+async function binanceSaveMarketWithSocket(marketables) {
 
     console.info("Start BinanceSaveMarketWithSocket BNNC...");
-    const marketables = await markettableList.getAllActualMarketable();
     let saveTemporalPairsBNNC = [];
 
     binance.websockets.prevDay(marketables, async (err, summary) => {
 
         if (err) return;
-        date_created = new Date();
-
         if (saveTemporalPairsBNNC.indexOf(summary.symbol) === -1) {
 
             saveTemporalPairsBNNC.push(summary.symbol);
-            let percent = parseFloat(summary.percentChange).toFixed(2);
+            date_created = new Date();
+            //let percent = parseFloat(summary.percentChange).toFixed(2);
             let currentData = {
                 broker: 3,
                 name: summary.symbol,
@@ -54,7 +55,7 @@ async function binanceSaveMarketWithSocket() {
                 volume: summary.quoteVolume,
                 mod: 0,
                 date_created: date_created,
-                percent: percent,
+                percent: 0,
             };
 
             await global.mongodb.saveItem("market", currentData);
@@ -62,31 +63,23 @@ async function binanceSaveMarketWithSocket() {
 
         if (marketables.length === saveTemporalPairsBNNC.length && !inside) {
             inside = true;
-            second_initial = date_created.getSeconds();
+            await sleep(15000);
+            inside = false;
+            saveTemporalPairsBNNC = [];
         }
-
-        if ((second_counter - second_initial) === 7) {
-            inside = false,
-                second_initial = 0,
-                second_counter = 0,
-                saveTemporalPairsBNNC = [];
-        }
-
-        second_counter = date_created.getSeconds();
     });
+}
 
-    await sleep(46000);
+async function stopSocket() {
+
+    await sleep(60000 * 60);
     let endpoints = binance.websockets.subscriptions();
     for (let endpoint in endpoints) {
-        console.log(endpoint);
         binance.websockets.terminate(endpoint);
     }
 
     console.info("End BinanceSaveMarketWithSocket BNNC...");
-    await sleep(10000);
-    inside = false,
-        second_initial = 0,
-        second_counter = 0;
+    await sleep(30000);
     timeToCallMarket();
 }
 
